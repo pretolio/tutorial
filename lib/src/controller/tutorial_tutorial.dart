59c7b0815f406c9131d7bb83461e0756c8d7647a
Gabriel Mattos <43770191+pretolio@users.noreply.github.com>
2021-10-14 00:01:10 -0300

    correcao nullface

--- lib/src/controller/tutorial_tutorial.dart
+++ lib/src/controller/tutorial_tutorial.dart
@@ -8,11 +8,11 @@ class Tutorial {
   static showTutorial(BuildContext context, List<TutorialItens> children, Function function) async {
     int count = 0;
     var size = MediaQuery.of(context).size;
-    OverlayState overlayState = Overlay.of(context);
+    OverlayState? overlayState = Overlay.of(context);
     List<OverlayEntry> entrys = [];
     children.forEach((element) async {
-      var offset = _capturePositionWidget(element.globalKey);
-      var sizeWidget = _getSizeWidget(element.globalKey);
+      Offset? offset = _capturePositionWidget(element.globalKey);
+      Size? sizeWidget = _getSizeWidget(element.globalKey);
       entrys.add(
         OverlayEntry(
           builder: (context) {
@@ -23,7 +23,7 @@ class Tutorial {
                       function(count);
                       count++;
                       if (count != entrys.length) {
-                        overlayState.insert(entrys[count]);
+                        overlayState!.insert(entrys[count]);
                       }
                     }
                   : () {},
@@ -35,10 +35,10 @@ class Tutorial {
                       size: size,
                       painter: HolePainter(
                           shapeFocus: element.shapeFocus,
-                          dx: (offset?.dx ?? 0) + ((sizeWidget?.width ?? size?.width) / 2),
-                          dy: (offset?.dy ?? 0) + ((sizeWidget?.height ?? size?.height) / 2),
-                          width: sizeWidget?.width ?? size?.width,
-                          height: sizeWidget?.height ?? size?.height),
+                          dx: (offset?.dx ?? 0) + ((sizeWidget?.width ?? size.width) / 2),
+                          dy: (offset?.dy ?? 0) + ((sizeWidget?.height ?? size.height) / 2),
+                          width: sizeWidget?.width ?? size.width,
+                          height: sizeWidget?.height ?? size.height),
                     ),
                     Positioned(
                       top: element.top,
@@ -46,12 +46,12 @@ class Tutorial {
                       left: element.left,
                       right: element.right,
                       child: Container(
-                        width: size?.width * 0.8,
+                        width: size.width * 0.8,
                         child: Column(
                           crossAxisAlignment: element.crossAxisAlignment,
                           mainAxisAlignment: element.mainAxisAlignment,
                           children: [
-                            ...element.children,
+                            ...?element.children,
                             GestureDetector(
                               child: element.widgetNext ??
                                   Text(
@@ -63,7 +63,7 @@ class Tutorial {
                                 function(count);
                                 count++;
                                 if (count != entrys.length) {
-                                  overlayState.insert(entrys[count]);
+                                  overlayState!.insert(entrys[count]);
                                 }
                               },
                             ),
@@ -80,23 +80,23 @@ class Tutorial {
       );
     });
     
-    overlayState.insert(entrys[0]);
+    overlayState!.insert(entrys[0]);
   }
 
 
-  static Offset _capturePositionWidget(GlobalKey key) {
+  static Offset? _capturePositionWidget(GlobalKey? key) {
     try{
       RenderBox renderPosition = key?.currentContext?.findRenderObject() as RenderBox;
-      return renderPosition?.localToGlobal(Offset.zero);
+      return renderPosition.localToGlobal(Offset.zero);
     }catch(e){
       return null;
     }
   }
 
-  static Size _getSizeWidget(GlobalKey key) {
+  static Size? _getSizeWidget(GlobalKey? key) {
     try{
       RenderBox renderSize = key?.currentContext?.findRenderObject() as RenderBox;
-      return renderSize?.size;
+      return renderSize.size;
     }catch(e){
       return null;
     }
